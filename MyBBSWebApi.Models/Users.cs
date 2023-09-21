d66230fa84241ef0af69095e69b347a4625441de
Chad <461755424@qq.com>
2023-09-21 13:05:31 +1200

    fixed some prob

--- MyBBSWebApi.Models/Users.cs
+++ MyBBSWebApi.Models/Users.cs
@@ -7,7 +7,7 @@
         public string UserNo { get; set; }
         public string UserName { get; set; }
         public int UserLevel { get; set; }
-        public string IsDelete { get; set; }
+        public string? IsDelete { get; set; }
         public string Password { get; set; }
         public Guid Token { get; set; }
         public Guid AutoLoginTag { get; set; }
